3cc53216270e79858c5db2fa9b69e64726411108
subramanyamvegiterralogic <subramanyam.vegi@terralogic.com>
2019-08-28 17:07:33 +0530

    Commit 1 on 2019-08-28

--- restapi1/class_based_views/urls.py
+++ restapi1/class_based_views/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url,include
 from class_based_views import views as ta3_views
 
 # Test Comment Added for testing purpose
+# EOD Commoit on 2019-08-28
 urlpatterns=[
     url(r'^api',ta3_views.EmployeeListCreateAPIView.as_view()),
     url(r'^api/(?P<id>\d)/$',ta3_views.EmployeeRetriveUpdatedestroyAPIView.as_view()),
